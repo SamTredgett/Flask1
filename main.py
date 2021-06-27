57e21a663779c1ce4c0ba11757fef1ea98ae04a4
Sam Tredgett <sam.tredgett@gmail.com>
2021-06-27 21:28:37 +0100

    implemented the fundamental structure for rendering first HTML templates in the templates folder and ran a quick test

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-
-
 @app.route("/")
 def home():
-    return "<h1>test home page</h1>"
+    return render_template("index.html") 
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
 
 
 if __name__ == "__main__":
