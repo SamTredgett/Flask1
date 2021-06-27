5a27381a96b1891fd56a78c7e720c9d66d7bdef4
Sam Tredgett <sam.tredgett@gmail.com>
2021-06-28 00:50:15 +0100

    minor changes to the main.py file, tidying up code and deleting comments

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
-from flask import Flask, redirect, url_for, render_template, request
+''' short backend project '''
+from flask import Flask, redirect, url_for, render_template, request, session
 
 app = Flask(__name__)
-
+app.secret_key = "secret_key"
 @app.route("/")
 def home():
+    ''' the home function renders the index.html view, the basic landing page '''
     return render_template("index.html")
 
 @app.route("/login", methods=["POST", "GET"]) 
 def login():
     if request.method == "POST":
         user = request.form["nm"] 
-        return redirect(url_for("user", usr=user))
+        session["user"] = user
+        return redirect(url_for("user"))
     else:
         return render_template("login.html")
 
-@app.route("/<usr>") 
-def user(usr):
-    return f"<h1>{usr}</h1>" 
-
-
+@app.route("/user") 
+def user():
+    ''' the user function simply returns a users name that is added posted by 
+        the login function.'''
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>" 
+    else:
+        return redirect(url_for("login"))
 if __name__ == "__main__":
     app.run(debug=True)
+
