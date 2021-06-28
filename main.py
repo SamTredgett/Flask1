0338b251bfa844373c3b50fa8b3977a405b5aa15
Sam Tredgett <sam.tredgett@gmail.com>
2021-06-28 01:20:31 +0100

    implemented better bootstrap into the login page and landing page, organised some of the content in the main.py file. Also wrote out some session tokens and created some permanent sessions for even after the user closes their browser

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
-''' short backend project '''
+''' short backend project utilising Flask '''
 from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "secret_key"
+app.permanent_session_lifetime = timedelta(days=1)
+
 @app.route("/")
 def home():
     ''' the home function renders the index.html view, the basic landing page '''
     return render_template("index.html")
 
-@app.route("/login", methods=["POST", "GET"]) 
+@app.route("/login", methods=["POST", "GET"])
 def login():
+    ''' login handles the POST and GET HTTP method requests for the form '''
     if request.method == "POST":
-        user = request.form["nm"] 
-        session["user"] = user
+        session.permanent = True
+        current_user = request.form["nm"]
+        session["user"] = current_user
         return redirect(url_for("user"))
-    else:
-        return render_template("login.html")
+    return render_template("login.html")
 
-@app.route("/user") 
+@app.route("/user")
 def user():
-    ''' the user function simply returns a users name that is added posted by 
+    ''' the user function simply returns a users name that is added posted by
         the login function.'''
     if "user" in session:
-        user = session["user"]
-        return f"<h1>{user}</h1>" 
-    else:
-        return redirect(url_for("login"))
+        current_user = session["user"]
+        return f"<h1>{current_user}</h1>"
+    return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    ''' removes user from the session data on the client side '''
+    session.pop("user",None)
+    return redirect(url_for("login"))
+
 if __name__ == "__main__":
     app.run(debug=True)
-
