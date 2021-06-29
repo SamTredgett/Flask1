1f9e88aa64d8fa5591a808c34b97559b8178f6fa
Sam Tredgett <sam.tredgett@gmail.com>
2021-06-29 17:31:39 +0100

    implemented the barebones of the SQLAlchemy database for storing user name and email, added a view.html view which shows the content of the database just to affirm that it functions correctly

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 A short experimental project refamiliarising myself with the flask framework.
 Currently in progress implementing the following:
  - Structure for logins using HTTP Requests that is secure and encrypted
- - Setting up SQL Alchemy in the project 
- - figuring out what kinda database design to use in this
+ - Moving on from basic SQLAlchemy implementation to a proper login format that 
+   value checks user input against the database and provides genuine authentication
+
--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 ''' short backend project utilising Flask '''
-import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.permanent_session_lifetime = timedelta(days=1)
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    email = db.Column(db.String(50))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+
+
 
 @app.route("/")
 def home():
@@ -19,6 +35,13 @@ def login():
         session.permanent = True
         current_user = request.form["nm"]
         session["user"] = current_user
+        user_found = users.query.filter_by(name=current_user).first()
+        if user_found:
+            session["email"] = user_found.email
+        else:
+            usr = users(current_user, "")
+            db.session.add(usr)
+            db.session.commit()
         flash("Login successful!")
         return redirect(url_for("user"))
     else:
@@ -35,10 +58,12 @@ def user():
     email = None
     if "user" in session:
         current_user = session["user"]
-
         if request.method == "POST":
             email = request.form["email"]
             session["email"] = email
+            user_found = users.query.filter_by(name=user).first()
+            user_found.email = email
+            db.session.commit()
             flash("Email was saved!")
         else:
             if "email" in session:
@@ -57,5 +82,13 @@ def logout():
     session.pop("user",None)
     return redirect(url_for("login"))
 
+
+@app.route("/view")
+def view():
+    return render_template("view.html", values=users.query.all())
+
+
+
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
