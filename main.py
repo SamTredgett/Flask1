f3c6c832253a21432a2a1b37a2399d9bcbec1211
Sam Tredgett <sam.tredgett@gmail.com>
2021-06-29 16:42:23 +0100

    updated the python in the user method to include post functionality and message flashing for the user if they succesfully add an email. Also updated the user.html view to include the front-end form functionality to complete this circuit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 ''' short backend project utilising Flask '''
+import sqlalchemy
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 
@@ -26,13 +27,23 @@ def login():
             return redirect(url_for("user"))
         return render_template("login.html")
 
-@app.route("/user")
+@app.route("/user", methods=["POST", "GET"])
 def user():
     ''' the user function simply returns a users name that is added posted by
         the login function.'''
+
+    email = None
     if "user" in session:
         current_user = session["user"]
-        return render_template("user.html", user = current_user) 
+
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            flash("Email was saved!")
+        else:
+            if "email" in session:
+                email = session["email"]
+        return render_template("user.html",email=email) 
     else:
         flash("You are not logged in!")
         return redirect(url_for("login"))
